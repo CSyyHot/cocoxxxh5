23d6b50141e8e4554760723c272d69359b955441
陈思源 <584930636@qq.com>
2026-03-17 17:41:31 +0800

    first commit

---
+++ assets/minigame_prefabs/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/minigame_prefabs",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/minigame_prefabs', 'chunks:///_virtual/minigame_prefabs'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
